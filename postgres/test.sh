e969ddf8dc628c2821f89d7f7cf2ba96bf077d59
Daksh <54345634+Dakshjain1@users.noreply.github.com>
2025-03-31 11:08:30 +0530

    tests run from script

--- postgres/test.sh
+++ postgres/test.sh
@@ -1,5 +1,6 @@
+export POSTGRES_PASSWORD=$(kubectl get secret --namespace test-ksp my-psql-postgresql -o jsonpath="{.data.postgres-password}" | base64 -d)
 kubectl port-forward --namespace test-ksp svc/my-psql-postgresql 5432:5432 &
-
+sleep 5
 PGPASSWORD="$POSTGRES_PASSWORD" psql --host 127.0.0.1 -U postgres -d postgres -p 5432 <<EOF
 SELECT 1;
 SELECT version();
