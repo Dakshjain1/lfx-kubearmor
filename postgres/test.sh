d79003b362c26d2c6b19948e5347652ae2080bb8
Daksh <54345634+Dakshjain1@users.noreply.github.com>
2025-03-31 20:32:46 +0530

    add new lines + fix ksp policy

--- postgres/test.sh
+++ postgres/test.sh
@@ -11,27 +11,3 @@ SELECT * FROM pg_stat_archiver;
 SELECT * FROM pg_stat_replication;
 SELECT NOW();
 EOF
-
-# PGPASSWORD="$POSTGRES_PASSWORD" psql --host 127.0.0.1 -U postgres -d postgres -p 5432
-
-
-# # Check connectivity by running a simple query
-# "SELECT 1;"
-
-# # Check PostgreSQL version
-# "SELECT version();"
-
-# # Check if tables exist
-# "\dt"
-
-# # Check active connections
-# "SELECT * FROM pg_stat_activity;"
-
-# # Check database size
-# "SELECT pg_size_pretty(pg_database_size('$DBNAME'));"
-
-# # Check the last backup (if applicable)
-# "SELECT * FROM pg_stat_archiver;"
-
-# "SELECT * FROM pg_stat_replication;"
-# SELECT NOW();
